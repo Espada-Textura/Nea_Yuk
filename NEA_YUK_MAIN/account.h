3a705f74834c4a5f7ae37372403295dbbeb62e84
Pisatto <76983137+NirPisatto@users.noreply.github.com>
2021-09-29 16:50:34 +0700

    add new

--- NEA_YUK_MAIN/account.h
+++ NEA_YUK_MAIN/account.h
@@ -10,10 +10,15 @@ class account;
 class account : public QWidget
 {
     Q_OBJECT
-
+    int number = 0;
 public:
     explicit account(QWidget *parent = nullptr);
     ~account();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
 
 private:
     Ui::account *ui;
